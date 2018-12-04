92b1d2ec2d492e261f61c6d39e4c13848d76c37c
Ryan Winchester <code@ryanwinchester.ca>
2018-12-03 22:42:09 -0800

    Test actual input

--- test/advent_test.exs
+++ test/advent_test.exs
@@ -1,7 +1,18 @@
 defmodule AdventTest do
   use ExUnit.Case
 
+  import Advent
+
   doctest Advent.Day1
   doctest Advent.Day2
   doctest Advent.Day3
+
+  test "day_1_1", do: assert(day_1_1() == 510)
+  test "day_1_2", do: assert(day_1_2() == 69074)
+
+  test "day_2_1", do: assert(day_2_1() == 8398)
+  test "day_2_2", do: assert(day_2_2() == "hhvsdkatysmiqjxunezgwcdpr")
+
+  test "day_3_1", do: assert(day_3_1() == 101_196)
+  test "day_3_2", do: assert(day_3_2() == 243)
 end
